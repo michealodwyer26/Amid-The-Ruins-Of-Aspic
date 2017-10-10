e706ff436ae6abacd3e9463c2e2b617a010b2e4f
michealodwyer26 <michealodwyer26@gmail.com>
2017-10-10 21:20:50 +0100

    Added Box2d to the game and rendered the debug lines of map objects

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -7,9 +7,19 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -21,6 +31,9 @@ public class GameScreen implements Screen {
 	private OrthogonalTiledMapRenderer mapRenderer;
 	private Viewport gamePort;
 	
+	private World world;
+	private Box2DDebugRenderer b2dr;
+	
 	public GameScreen(Platformer game) {		
 		this.game = game;
 		
@@ -31,6 +44,31 @@ public class GameScreen implements Screen {
 		mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / Platformer.PPM);
 		
 		gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2f + (384 / Platformer.PPM), 0); // Adds an amount in m to set the camera at the right y value
+		
+		world = new World(new Vector2(0, -10), true);
+		b2dr = new Box2DDebugRenderer();
+		b2dr.SHAPE_STATIC.set(1, 0, 0, 1);
+		
+		BodyDef bdef = new BodyDef();
+		PolygonShape shape = new PolygonShape();
+		FixtureDef fdef = new FixtureDef();
+		Body body;
+		
+		// Get the ground Map Objects
+		
+		for(MapObject object : map.getLayers().get("Ground").getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set((rect.getX() + rect.getWidth()/2) / Platformer.PPM, (rect.getY() + rect.getHeight()/2) / Platformer.PPM);
+			
+			body = world.createBody(bdef);
+			
+			shape.setAsBox(rect.getWidth() / 2 / Platformer.PPM, rect.getHeight() / 2 / Platformer.PPM);
+			fdef.shape = shape; 
+			
+			body.createFixture(fdef);
+		}
 		
 	}
 
@@ -44,6 +82,10 @@ public class GameScreen implements Screen {
 			gamecam.translate(-0.02f, 0);
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
 			gamecam.translate(0.02f, 0);
+		if(Gdx.input.isKeyPressed(Input.Keys.UP))
+			gamecam.translate(0, 0.02f);
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+			gamecam.translate(0, -0.02f);
 	}
 
 	@Override
@@ -57,6 +99,9 @@ public class GameScreen implements Screen {
 		
 		mapRenderer.setView(gamecam);
 		mapRenderer.render();
+		
+		b2dr.render(world, gamecam.combined);
+		
 		
 	}
 
